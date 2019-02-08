afe7ec5e2e4ce731b324df724cbfdbeb9cef982b
oskarpihlak <oskar.pihlak@gmail.com>
2019-02-08 18:15:26 +0200

    message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Heading from './components/Header/Heading.js';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <title>Küberturbe alused!</title>
         </header>
+          <div className="container">
+            <Heading/>
+          </div>
       </div>
     );
   }
