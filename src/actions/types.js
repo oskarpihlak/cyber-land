67637476cc9c4c37fb414dd9ab45d8359bd08b9b
oskarpihlak <oskar.pihlak@gmail.com>
2019-02-16 12:43:29 +0200

    repository redesign for heroku

--- src/actions/types.js
+++
@@ -1,7 +0,0 @@
-export const OTP = 'OTP';
-export const SHA2 = 'SHA2';
-export const MD5 = 'MD5';
-
-export const CHANGE_CRYPTION_TYPE = 'CHANGE_CRYPTION_TYPE';
-export const CHANGE_CRYPTION = 'CHANGE_CRYPTION';
-export const INITIAL_STATE = 'INITIAL_STATE';
