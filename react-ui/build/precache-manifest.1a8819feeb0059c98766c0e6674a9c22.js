1ab0e6140869b3f4b8d94686c829ed4aa2185725
Oskar Oihlak <Oskar.Pihlak@nortal.com>
2019-02-27 20:08:51 +0200

    repository redesign for heroku

---
+++ react-ui/build/precache-manifest.1a8819feeb0059c98766c0e6674a9c22.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "457b8dd61639c0ce538ec90739277c42",
+    "url": "/static/media/cypto1.457b8dd6.jpg"
+  },
+  {
+    "revision": "988f840deb5205218edca3d09a9db141",
+    "url": "/static/media/crypto3.988f840d.jpg"
+  },
+  {
+    "revision": "9eaceb2cf5f5013c6183c74a6902abb2",
+    "url": "/static/media/crypto2.9eaceb2c.jpg"
+  },
+  {
+    "revision": "97772b44b792afae788864820c64d097",
+    "url": "/static/media/Threat-768x553.97772b44.jpg"
+  },
+  {
+    "revision": "a6cdd4bda1768c15a455d4dfe642fe4a",
+    "url": "/static/media/Cyber-Security-PNG-Background-Image.a6cdd4bd.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "04691c8543358a21c13b",
+    "url": "/static/js/main.04691c85.chunk.js"
+  },
+  {
+    "revision": "ed7f4f0743027f428f15",
+    "url": "/static/js/2.ed7f4f07.chunk.js"
+  },
+  {
+    "revision": "04691c8543358a21c13b",
+    "url": "/static/css/main.b68da261.chunk.css"
+  },
+  {
+    "revision": "fb8ebf91742079a2edaaa418d1d3e2e5",
+    "url": "/index.html"
+  }
+];
